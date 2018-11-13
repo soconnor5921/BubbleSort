cc1e25e09b5b4dff3f2c648182cc2c13ff435363
BT_1N3_13@bktech.local <BT_1N3_13@bktech.local>
2018-11-13 10:15:57 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+
+    public static int[] swap(int[] array, int pos1, int pos2)
+    {
+        int temp = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = temp;
+        return array;
+    }
+
+    public void bubbleSort(int[] arr)
+    {
+        boolean running = true;
+        while(running)
+        {
+            
+        }
+    }
+}
